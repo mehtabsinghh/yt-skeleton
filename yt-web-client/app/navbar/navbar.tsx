e243dfcb53836cc52ef08e35961d71b089ecbf57
Mehtab <mehtabsinghxx@gmail.com>
2023-09-30 14:36:22 +0100

    Add create user function

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,15 +1,38 @@
+'use client';
 import Image from "next/image";
+import SignIn from "./sign-in";
 import Link from "next/link";
 
 import styles from "./navbar.module.css";
+import { useEffect, useState } from "react";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
 
-export default function Navbar() {
+
+function NavBar() {
+  // Initialize user state
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [] /* No dependencies, never rerun */);
+
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <Image width={90} height={20}
-          src="/youtube-logo.svg" alt="YouTube Logo"/>
+        <span className={styles.logoContainer}>
+          <Image width={90} height={20} className={styles.logo} src="/youtube-logo.svg" alt="YouTube Logo" />
+        </span>
       </Link>
+      <SignIn user={user} />
     </nav>
   );
 }
+
+export default NavBar;
